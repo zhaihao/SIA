3e9a1b033ecd05d8e8673f8286a400797163d6ed
zhaihao <zhaihao@ooon.me>
2019-08-28 18:47:24 +0800

    feat: spark 创建 struct

---
+++ src/test/scala/sia/spark/sql/StructSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2019.
+ * OOON.ME ALL RIGHTS RESERVED.
+ * Licensed under the Mozilla Public License, version 2.0
+ * Please visit http://ooon.me or mail to zhaihao@ooon.me
+ */
+
+package sia.spark.sql
+
+import sia.spark.SparkSpec
+import test.BaseSpec
+
+/**
+  * StructSpec
+  *
+  * @author zhaihao
+  * @version 1.0
+  * @since 2019/8/28 5:30 下午
+  */
+class StructSpec extends SparkSpec {
+  import spark.implicits._
+  "构造一个 struct" in {
+    Seq(
+      (1, "a", 31),
+      (2, "b", 32),
+      (3, "c", 33),
+      (4, "d", 34)
+    ).toDF("id", "name", "age").createOrReplaceTempView("t_user")
+
+    // language=SQL
+    val res = spark.sql("""
+                          |select
+                          |  id,
+                          |  struct(name,age,1 as l) as j
+                          |from t_user
+                          |""".stripMargin)
+    
+    res.show()
+    res.printSchema()
+  }
+}
